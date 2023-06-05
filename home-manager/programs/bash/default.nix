b9e25a057bfa3f79695d30c36f8b9e7caee0f82d
martin-lndbl-GTL <lblsolutions@outlook.de>
2023-06-05 09:08:06 +0200

    shell alias to remove all upstream deleted branches

--- home-manager/programs/bash/default.nix
+++ home-manager/programs/bash/default.nix
@@ -16,6 +16,7 @@
       c = "clear";
       switch = "home-manager switch --flake ~/.config/nixos-system-config#mrtn@$(hostname)";
       rebuild = "sudo nixos-rebuild switch --flake ~/.config/nixos-system-config#$(hostname)";
+      gba = "git fetch -p && git branch -vv | grep gone | cut -d' ' -f 3 | grep . | xargs git branch -D";
     };
   };
 }
