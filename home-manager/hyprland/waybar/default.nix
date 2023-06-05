0c84928a5fb4f91d2271a74e9f48d4e950085411
martin-lndbl-GTL <lblsolutions@outlook.de>
2023-06-05 07:57:31 +0200

    generalized waybar config

--- home-manager/hyprland/waybar/default.nix
+++ home-manager/hyprland/waybar/default.nix
@@ -1,93 +1,93 @@
-{ pkgs }:
+{ pkgs, config, lib, ... }:
 
 {
-  enable = true;
-  style = builtins.readFile ./styles.css;
-  settings = {
-    mainBar = {
-      layer = "top";
-      position = "bottom";
-      height = 42;
-      output = [
-        "eDP-1"
-        "DP-1"
-      ];
+  programs.waybar =
+    {
+      enable = true;
+      style = import ./style.nix { inherit config; };
+      settings = {
+        mainBar = {
+          layer = "top";
+          position = "bottom";
+          height = config.appearance.fontSize*3;
+          output = lib.attrValues config.monitors;
 
-      modules-left = [
-        "wlr/workspaces"
-      ];
-      modules-center = [
-        /* "wlr/taskbar" */
-        "clock"
-      ];
+          modules-left = [
+            "wlr/workspaces"
+          ];
+          modules-center = [
+            /* "wlr/taskbar" */
+            "clock"
+          ];
 
-      modules-right = [
-        "pulseaudio"
-        "network"
-        "cpu"
-        "memory"
-        "disk"
-        "battery"
-      ];
+          modules-right = [
+            "pulseaudio"
+            "network"
+            "cpu"
+            "memory"
+            "disk"
+            "battery"
+          ];
 
-      "wlr/workspaces" = {
-        disable-scroll = true;
-        all-outputs = true;
-        on-click = "activate";
-        format = "{icon}";
-        format-icons = {
-          "1" = "1";
-          "2" = "2";
-          "3" = "3";
-          "4" = "4";
-          "5" = "5";
-          "6" = "6";
-          "7" = "7";
-          "8" = "8";
-          "9" = "9";
-          "10" = "10";
+          "wlr/workspaces" = {
+            disable-scroll = true;
+            all-outputs = true;
+            on-click = "activate";
+            format = "{icon}";
+            format-icons = {
+              "1" = "1";
+              "2" = "2";
+              "3" = "3";
+              "4" = "4";
+              "5" = "5";
+              "6" = "6";
+              "7" = "7";
+              "8" = "8";
+              "9" = "9";
+              "10" = "10";
+            };
+          };
+          "wlr/mode" = {
+            format = "<span style=\"italic\">{}</span>";
+          };
+
+          "pulseaudio" = {
+            format = "🔊 {volume}%";
+            format-muted = "🔈 {volume}%";
+          };
+
+          "network" = {
+            format = "Disabled";
+            format-wifi = "WIFI: {essid}";
+            format-ethernet = "E: {ipaddr}";
+            format-disconnected = "Disconnected";
+          };
+          "cpu" = {
+            format = "CPU: {usage}%";
+            on-click = "alacritty -e htop";
+          };
+          "memory" = {
+            format = "RAM: {}%";
+            on-click = "alacritty -e htop";
+          };
+          "clock" = {
+            format = "{:%a %d %b - %H:%M}";
+            interval = 1;
+          };
+          "disk" = {
+            format = "{free}";
+          };
+
+          "battery" = {
+            states = {
+              good = 95;
+              warning = 30;
+              critical = 15;
+            };
+            format = "🔋 {capacity: >3}%";
+            format-charging = "⚡{capacity}%";
+          };
         };
-      };
-      "wlr/mode" = {
-        format = "<span style=\"italic\">{}</span>";
-      };
-
-      "pulseaudio" = {
-        format = "🔊 {volume}%";
-        format-muted = "🔈 {volume}%";
-      };
-
-      "network" = {
-        format = "Disabled";
-        format-wifi = "WIFI: {essid}";
-        format-ethernet = "E: {ipaddr}";
-        format-disconnected = "Disconnected";
-      };
-      "cpu" = {
-        format = "CPU: {usage}%";
-        on-click = "alacritty -e htop";
-      };
-      "memory" = {
-        format = "RAM: {}%";
-        on-click = "alacritty -e htop";
-      };
-      "clock" = {
-        format = "{:%a %d %b - %H:%M}";
-        interval = 1;
-      };
-      "disk" = {
-        format = "{free}";
-      };
-
-      "battery" = {
-        states = {
-          good = 95;
-          warning = 30;
-          critical = 15;
-        };
-        format = "🔋 {capacity: >3}%";
-        format-charging = "⚡{capacity}%";
       };
     };
-  };
 }
