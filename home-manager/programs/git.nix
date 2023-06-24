89a13873f4ccd0f2e5ae48a2dd4240f24b3f7cef
martin-lndbl-GTL <lblsolutions@outlook.de>
2023-06-24 17:37:27 +0200

    git allow

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -8,9 +8,12 @@
     userName = "martin-lndbl-GTL";
     userEmail = "lblsolutions@outlook.de";
     extraConfig.safe.directory = [
-      "/D/docs/03_sem"
-      "/D/docs/04_sem"
+      "/D/docs/uni/03_sem"
+      "/D/docs/uni/04_sem"
       "/D/docs/uni/ASP/task4-concurrency-Martin-Lndbl"
+      "/D/docs/uni/ASP/task5-memory-Martin-Lndbl"
+      "/D/docs/uni/ASP/task6-sockets-Martin-Lndbl"
+      "/D/docs/uni/ASP/task7-performance-Martin-Lndbl"
     ];
   };
 }
