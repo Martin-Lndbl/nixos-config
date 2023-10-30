265431d6cfe6c944cd9deb3557ccda024f2d9985
martin-lndbl-GTL <lblsolutions@outlook.de>
2023-10-30 15:25:55 +0900

    base16 spacecamp copy

---
+++ home-manager/colorschemes/spacecamp.nix
@@ -0,0 +1,23 @@
+{
+  slug = "spacecamp";
+  name = "SpaceCamp";
+  author = "https://github.com/jaredgorski/SpaceCamp";
+  colors = {
+    base00 = "#282828";
+    base01 = "#D71A1A";
+    base02 = "#57BA37";
+    base03 = "#F0D50C";
+    base04 = "#91AADF";
+    base05 = "#CF73E6";
+    base06 = "#B7CBF4";
+    base07 = "#DEDEDE";
+    base08 = "#666666";
+    base09 = "#FF0000";
+    base0A = "#D8FA3B";
+    base0B = "#E7C547";
+    base0C = "#B7CBF4";
+    base0D = "#B77EE0";
+    base0E = "#A9C1DE";
+    base0F = "#EEEEEE";
+  };
+}
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,7 +6,7 @@
   home.username = "mrtn";
   home.homeDirectory = "/home/mrtn";
 
-  colorScheme = inputs.nix-colors.colorSchemes.dracula;
+  colorScheme = import ./colorschemes/spacecamp.nix;
 
   nixpkgs = {
     overlays = outputs.overlays.modifications ;
