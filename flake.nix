17901b39681f56bdbeae25836232776480551b52
martin-lndbl-GTL <lblsolutions@outlook.de>
2023-10-30 14:33:04 +0900

    background image

--- flake.nix
+++ flake.nix
@@ -91,7 +91,7 @@
             ./home-manager/home.nix
             ./home-manager/hyprland
             {
-              config.appearance.wallpaper = "~/downloads/wallpaper/jp2.png";
+              config.appearance.wallpaper = "~/downloads/wallpaper/polaris.jpg";
               config.appearance.lockScreen = "~/downloads/wallpaper/nix.png";
               config.appearance.fontSize = 14;
               config.monitors.center = "eDP-1";
