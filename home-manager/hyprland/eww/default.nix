f777d531bab72d93b27e4ccacbb6f1fde865e99f
martin-lndbl-GTL <lblsolutions@outlook.de>
2023-11-01 17:41:40 +0900

    nix config usable in scss files

--- home-manager/hyprland/eww/default.nix
+++ home-manager/hyprland/eww/default.nix
@@ -8,11 +8,12 @@
   xdg.configFile."eww/eww.yuck".source = pkgs.writeText "eww.yuck"
     "${builtins.readFile ./config_dir/eww.yuck}";
 
-  xdg.configFile."eww/eww.scss".source = pkgs.writeText "eww.yuck"
-    "${builtins.readFile ./config_dir/eww.scss}";
+  xdg.configFile."eww/eww.scss".source = import ./config_dir/eww.scss.nix {
+    inherit pkgs config;
+  };
 
-    xdg.configFile."eww/scripts" = {
-      source = ./config_dir/scripts;
-      recursive = true;
-    };
+  xdg. configFile."eww/scripts" = {
+    source = ./config_dir/scripts;
+    recursive = true;
+  };
 }
