ec1d239a06f385743752d510040afddcfb2d14a0
martin-lndbl-GTL <lblsolutions@outlook.de>
2023-11-16 21:18:51 +0900

    init

--- home-manager/games.nix
+++ home-manager/games.nix
@@ -1,23 +1,7 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    # gamemode
     vitetris
     atlauncher
   ];
-
-  # programs.atlauncher = {
-  #   enable = false;
-  #   package = pkgs.unstable.atlauncher;
-
-  #   settings = {
-  #     enableAnalytics = true;
-  #     enableConsole = false;
-  #     enableTrayMenu = false;
-  #     firstTimeRun = false;
-  #     keepLauncherOpen = false;
-  #     usingCustimJavaPath = true;
-  #     javaPath = "${pkgs.jdk8}";
-  #   };
-  # };
 }
