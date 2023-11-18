c96487f138ad9e6588d6044e1777ad0eb72d400e
martin-lndbl-GTL <lblsolutions@outlook.de>
2023-11-18 13:50:27 +0900

    at launcher vanilla fix

--- home-manager/games.nix
+++ home-manager/games.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-   # gamemode
+    # gamemode
     vitetris
     atlauncher
   ];
@@ -18,16 +18,6 @@
   #     keepLauncherOpen = false;
   #     usingCustimJavaPath = true;
   #     javaPath = "${pkgs.jdk8}";
-  #   };
-  # };
-  # xdg.desktopEntries = {
-  #   rocket-league = {
-  #     categories = [ "Game" ];
-  #     exec = "xdg-open heroic://launch/legendary/Sugar";
-  #     icon = "/home/getpsyched/.config/heroic/icons/Sugar.jpg";
-  #     name = "Rocket League";
-  #     terminal = false;
-  #     type = "Application";
   #   };
   # };
 }
