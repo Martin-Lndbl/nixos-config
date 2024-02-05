29adc1d742d10941a6c8440db8d850d798ddc646
martin-lndbl-GTL <lblsolutions@outlook.de>
2024-02-05 17:51:09 +0900

    fomat fix users

--- nixos/users.nix
+++ nixos/users.nix
@@ -3,8 +3,13 @@
   users.users.mrtn = {
     isNormalUser = true;
     initialPassword = "pwd";
-    extraGroups = [ "wheel" "video" "audio" "scanner" "networkmanager" "docker" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
+    extraGroups = [
+      "wheel"
+      "video"
+      "audio"
+      "scanner"
+      "networkmanager"
+      "docker"
     ];
   };
 }
