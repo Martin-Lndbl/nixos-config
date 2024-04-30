de1e503188f51f71838817b57631741acdc71aed
martin-lndbl-GTL <lblsolutions@outlook.de>
2024-04-30 16:33:19 +0200

    nix-gt wallpaper config

--- hm/users/mrtn/nix-gt.nix
+++ hm/users/mrtn/nix-gt.nix
@@ -1,7 +1,20 @@
-{ inpugs, pkgs, lib, config, ... }:
+{ inputs, pkgs, lib, config, ... }:
 
+let
+  inherit (inputs.nix-colors.lib-contrib { inherit pkgs; })
+    nixWallpaperFromScheme;
+in
 {
   imports = [ ./secrets.nix ];
+
+  config.appearance.wallpaper = nixWallpaperFromScheme {
+    scheme = config.colorscheme;
+    width = 1920;
+    height = 1080;
+    logoScale = 8;
+    logoColor1 = config.colorscheme.palette.base03;
+    logoColor2 = config.colorscheme.palette.base04;
+  };
 
   config.appearance.fontSize = 18;
   config.monitors.center = "DP-2";
