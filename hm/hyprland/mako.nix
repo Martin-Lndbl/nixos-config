b94d3c421edd9fcd2c97907fbec8384f8158cb83
martin_lndbl-NBL <lblsolutions@outlook.de>
2025-06-05 08:32:54 +0200

    flake upadte

--- hm/hyprland/mako.nix
+++ hm/hyprland/mako.nix
@@ -1,15 +1,17 @@
-{ pkgs, config, ... }:
+{ config, ... }:
 
 {
   services.mako = with config.colorScheme.palette; {
     enable = true;
-    anchor = "top-center";
-    height = config.appearance.fontSize * 5;
-    width = config.appearance.fontSize * 20;
-    defaultTimeout = 10000;
-    borderRadius = config.appearance.fontSize;
-    backgroundColor = "#${base06}";
-    borderColor = "#${base00}";
-    textColor = "#${base00}";
+    settings = {
+      anchor = "top-center";
+      height = config.appearance.fontSize * 5;
+      width = config.appearance.fontSize * 20;
+      defaultTimeout = 10000;
+      borderRadius = config.appearance.fontSize;
+      backgroundColor = "#${base06}";
+      borderColor = "#${base00}";
+      textColor = "#${base00}";
+    };
   };
 }
