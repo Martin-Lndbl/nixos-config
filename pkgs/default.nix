c0122e3b7813c3e688e32a1c8cac9a6ee27fcff6
martin_lndbl-NBL <lblsolutions@outlook.de>
2025-07-24 13:10:28 +0200

    propper trilium desktop packaging

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,11 @@
-{ pkgs ? (import ../nixpkgs.nix) { } }: {
+{
+  pkgs ? (import ../nixpkgs.nix) { },
+}:
+{
   # example = pkgs.callPackage ./example { };
   ape = pkgs.callPackage ./ApE { };
   atlauncher = pkgs.callPackage ./atlaucher { };
   hyprctl-rotate = pkgs.callPackage ./hyprctl-rotate.nix { };
   hm = pkgs.home-manager;
+  trilium-next-desktop = pkgs.callPackage ./trilium/trilium-next-desktop.nix { };
 }
