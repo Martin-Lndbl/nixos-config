c7424d99defd0269a9132e935d2694ffedab50bb
martin_lndbl-NBL <lblsolutions@outlook.de>
2025-11-05 13:45:34 +0100

    adjust cpu config for nix-nb

--- nixos/machines/nix-nb.nix
+++ nixos/machines/nix-nb.nix
@@ -1,39 +1,48 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
 
 {
-  imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   networking.hostName = "nix-nb";
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "thunderbolt"
+    "vmd"
+    "nvme"
+    "usb_storage"
+    "sd_mod"
+  ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/0d6b3afe-378c-40fe-811f-6bb7a68fa247";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/0d6b3afe-378c-40fe-811f-6bb7a68fa247";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/C89E-0889";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/C89E-0889";
+    fsType = "vfat";
+  };
 
   swapDevices = [ ];
 
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   boot.extraModprobeConfig = lib.mkMerge [
@@ -47,8 +56,19 @@
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      # Reduce sustained and short-term turbo power limits (Watts)
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+      CPU_MAX_PERF_ON_AC = 80; # percent, 100 = full, 80 = cooler
+      CPU_BOOST_ON_AC = 1; # keep turbo on but limited
+
+      # Platform profiles
+      PLATFORM_PROFILE_ON_AC = "balanced";
     };
   };
+
+  services.thermald.enable = true;
+  powerManagement.cpuFreqGovernor = "performance";
 
   services.udev.extraRules = lib.mkMerge [
     # autosuspend USB devices
