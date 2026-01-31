efc3eba902fe0810998ebbf6fa883b730f72180e
Martin-Lndbl <>
2026-01-31 17:14:46 +0100

    move hyprpanel to own file

--- hm/hyprland/default.nix
+++ hm/hyprland/default.nix
@@ -1,66 +1,17 @@
 {
   pkgs,
-  config,
   ...
 }:
 {
   imports = [
     ./hyprland.nix
     ./hyprlock.nix
+    ./hyprpanel.nix
     ./wofi.nix
     ./grim.nix
   ];
 
   stylix.targets.hyprland.enable = true;
-  stylix.targets.hyprpanel.enable = true;
-  programs.hyprpanel = {
-    enable = true;
-    settings = {
-      bar.bluetooth.label = false;
-      bar.clock.format = "%a %b %d %H:%M:%S";
-      bar.layouts."${config.monitors.center}" = {
-        left = [
-          "dashboard"
-          "workspaces"
-          "windowtitle"
-        ];
-        middle = [
-          "media"
-        ];
-        right = [
-          "volume"
-          "network"
-          "clock"
-          "systray"
-          (if config.appearance.hasBattery then "battery" else "")
-          "notifications"
-        ];
-      };
-      bar.layouts."${config.monitors.right}" = {
-        left = [
-          "dashboard"
-          "workspaces"
-          "windowtitle"
-        ];
-        middle = [
-          "media"
-        ];
-        right = [
-          "volume"
-          "clock"
-          (if config.appearance.hasBattery then "battery" else "")
-          "notifications"
-        ];
-      };
-
-      theme = {
-        font.size = config.appearance.fontSize;
-        bar.transparent = true;
-        bar.location = "bottom";
-      };
-    };
-  };
-
   home.packages = with pkgs; [
     wl-clipboard
     hyprctl-rotate
--- hm/hyprland/hyprlock.nix
+++ hm/hyprland/hyprlock.nix
@@ -1,15 +1,26 @@
-{ config,... }:
-
+{ config, lib, ... }:
+let
+  hexToRgbA =
+    with lib.string;
+    hex:
+    let
+      r = toInt (substring 0 2 hex) 16;
+      g = toInt (substring 2 4 hex) 16;
+      b = toInt (substring 4 6 hex) 16;
+      a = 100;
+    in
+    "rgba(${toString r}, ${toString g}, ${toString b}, ${toString a})";
+in
 {
   stylix.targets.hyprlock.enable = false;
   stylix.targets.hyprlock.colors.enable = false;
   programs.hyprlock = {
     enable = true;
-    extraConfig = with config.lib.stylix.colors;''
-      $base = #${base0A}
-      $accent = #${base0E}
-      $accentAlpha = #${base07}
-      $text = #${base01}
+    extraConfig = with config.lib.stylix.colors; ''
+      $base = "#${base0A}"
+      $accent = "#${base0E}"
+      $accentAlpha = "#${base07}"
+      $text = "#${base01}"
 
       general {
           hide_cursor = true
@@ -50,13 +61,13 @@
           dots_spacing = 0.2
           dots_center = true
           outer_color = $accent
-          inner_color = #${base08}
+          inner_color = "#${base08}"
           font_color = $text
           fade_on_empty = false
           placeholder_text = <span><i>Password...</i></span>
           hide_input = false
           check_color = $accent
-          fail_color = #${base04}
+          fail_color = "#${base04}"
           fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i>
           capslock_color = $yellow
           position = 0, -35
