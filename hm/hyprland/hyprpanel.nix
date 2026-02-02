b19618d16040fc0731f02ec2c9b43d50e82a2780
Martin-Lndbl <>
2026-02-02 16:42:12 +0100

    improve hyprpanel

--- hm/hyprland/hyprpanel.nix
+++ hm/hyprland/hyprpanel.nix
@@ -1,9 +1,34 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
+
+  home.packages = with pkgs; [
+    btop
+    power-profiles-daemon
+    wf-recorder
+  ];
+
   stylix.targets.hyprpanel.enable = true;
   programs.hyprpanel = {
     enable = true;
     settings = {
+      menus = {
+        dashboard = {
+          powermenu.avatar.image = config.appearance.profile.picture;
+          recording.path = "${config.xdg.userDirs.videos}/screenrecording";
+          directories.enabled = false;
+        };
+        clock = {
+          time = {
+            military = "%H:%M:%S";
+            hideSeconds = false;
+          };
+          weather = {
+            enabled = true;
+            unit = "metric";
+            location = "Munich";
+          };
+        };
+      };
       bar.launcher.autoDetectIcon = true;
       bar.bluetooth.label = false;
       bar.clock.format = "%a %b %d %H:%M:%S";
