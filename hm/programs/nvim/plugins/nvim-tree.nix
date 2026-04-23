afc1e137e69f24f1aa14b90f125bb0274933759c
Martin-Lindbuechl@cronus <lblsolutions@outlook.com>
2026-04-23 10:13:16 +0200

    update nvimtree config

--- hm/programs/nvim/plugins/nvim-tree.nix
+++ hm/programs/nvim/plugins/nvim-tree.nix
@@ -1,32 +1,17 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 
 {
-  programs.neovim.plugins = [{
-    plugin = pkgs.vimPlugins.nvim-tree-lua;
-    type = "lua";
-    config = ''
-      local treeapi = require "nvim-tree.api"
-      vim.keymap.set('n', '<leader>t', treeapi.tree.toggle)
-      local function my_on_attach(bufnr)
-        local function opts(desc)
-          return {
-            desc = "nvim-tree: " .. desc,
-            buffer = bufnr,
-            noremap = true,
-            silent = true,
-            nowait = true
-          }
-        end
-
-        treeapi.config.mappings.default_on_attach(bufnr)
-      end
-
-      require("nvim-tree").setup {
-        on_attach = my_on_attach,
-        filters = {
-          dotfiles = true,
-        },
-      }
-    '';
-  }];
+  programs.neovim.plugins = [
+    {
+      plugin = pkgs.vimPlugins.nvim-tree-lua;
+      type = "lua";
+      config =
+        #lua
+        ''
+          require("nvim-tree").setup()
+          local nvimtreeapi = require("nvim-tree.api")
+          vim.keymap.set("n", "<leader>t", nvimtreeapi.tree.toggle, { desc = "Toggle NvimTree" })
+        '';
+    }
+  ];
 }
