c368c2c81479af2cd0ab5f32b430751bda26a92c
Martin-Lindbuechl@cronus <lblsolutions@outlook.com>
2026-05-25 00:42:40 +0200

    fix local dns config

--- nixos/wireguard.nix
+++ nixos/wireguard.nix
@@ -12,6 +12,13 @@
     resolveLocalQueries = true;
     settings = {
       server = [
+        # Public Endpoints
+        "/gateway.lndbl.de/8.8.8.8"
+        "/matrix.lndbl.de/8.8.8.8"
+        "/mail.lndbl.de/8.8.8.8"
+        "/push.lndbl.de/8.8.8.8"
+        "/www.lndbl.de/8.8.8.8"
+
         # --- Pyroeis VPN ---
         "/lndbl.de/172.16.0.1"
         "/16.172.in-addr.arpa/172.16.0.1"
@@ -19,12 +26,16 @@
         # --- Eos VPN ---
         "/home/10.10.0.1"
         "/10.10.in-addr.arpa/10.10.0.1"
+
+        "8.8.8.8"
+        "8.8.4.4"
       ];
       domain-needed = true;
       bogus-priv = true;
     };
   };
 
+  networking.firewall.checkReversePath = false;
   networking.firewall = {
     # if packets are still dropped, they will show up in dmesg
     logReversePathDrops = true;
