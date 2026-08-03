e87c660aea117ce386240e5c9678f917f6ed071c
Martin Lindbüchl <martin.lindbuechl@tum.de>
2026-08-03 14:45:36 +0000

    2

--- nixos/wm/sddm.nix
+++ nixos/wm/sddm.nix
@@ -1,5 +1,16 @@
 { pkgs, ... }:
 
+let
+  avatar = pkgs.fetchurl {
+    url = "https://avatars.githubusercontent.com/u/77677509?v=4";
+    hash = "sha256-xUB6FICXhoX8lK/tZI9yiVAY2VFuKXePwGnhQhKHWg0=";
+  };
+  facesDir = pkgs.runCommand "sddm-faces" { } ''
+    mkdir -p $out
+    cp ${avatar} $out/mrtn.face.icon
+    cp ${avatar} $out/.face.icon
+  '';
+in
 {
   services.xserver.enable = true;
   services.displayManager.sddm = {
@@ -9,13 +20,16 @@
     extraPackages = with pkgs; [
       kdePackages.qtmultimedia
       kdePackages.qtsvg
-      kdePackages.qtvirtualkeyboard
       kdePackages.qt5compat
     ];
     settings = {
       Theme = {
         CursorTheme = "phinger-cursors-light";
         CursorSize = 28;
+        FacesDir = "${facesDir}";
+      };
+      General = {
+        InputMethod = "";
       };
     };
   };
