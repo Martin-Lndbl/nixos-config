638a1433eb9c7c278c9d8e5f465903b6df64a9ff
Martin Lindbüchl <martin.lindbuechl@tum.de>
2026-08-03 12:10:07 +0000

    caelestia update

--- hm/hyprland/caelestia.nix
+++ hm/hyprland/caelestia.nix
@@ -3,12 +3,18 @@
 {
   imports = [ inputs.caelestia-shell.homeManagerModules.default ];
 
+  stylix.targets.gtk.enable = false;
+  stylix.targets.qt.enable = false;
+  stylix.targets.fuzzel.enable = false;
+  stylix.targets.hyprland.enable = false;
+
   programs.caelestia = {
     enable = true;
     systemd = {
       enable = true;
       target = "graphical-session.target";
     };
+    settings.services.smartScheme = false;
     cli.enable = true;
   };
 }
